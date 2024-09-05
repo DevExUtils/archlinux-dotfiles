3e584ed4fe3bfe2422139a503cc29cf1fe8d5f1e
Henrik Stanley Mortensen <31671391+HenrikStanley@users.noreply.github.com>
2024-09-06 00:23:21 +0200

    refactor: change function to use yay instead of paru

--- home/dot_config/zsh/settings/executable_09-functions.zsh
+++ home/dot_config/zsh/settings/executable_09-functions.zsh
@@ -1,19 +1,19 @@
 #!/bin/zsh
 
-# FZF Paru 
+# FZF Yay 
 ##  Search and install packages with fzf
 pi() { 
-	SELECTED_PKGS="$(paru -Slq | fzf --header='Install packages' -m --preview 'paru -Si {1}')"
+	SELECTED_PKGS="$(yay -Slq | fzf --header='Install packages' -m --preview 'yay -Si {1}')"
 	if [ -n "$SELECTED_PKGS" ]; then
-		paru -S "$SELECTED_PKGS"
+		yay -S "$SELECTED_PKGS"
 	fi
 }
 
 ## Search and remove installed packages with fzf
 pr() { 
-	SELECTED_PKGS="$(paru -Qsq | fzf --header='Remove packages' -m --preview 'paru -Si {1}')"
+	SELECTED_PKGS="$(yay -Qsq | fzf --header='Remove packages' -m --preview 'yay -Si {1}')"
 	if [ -n "$SELECTED_PKGS" ]; then
-		paru -Rns "$SELECTED_PKGS"
+		yay -Rns "$SELECTED_PKGS"
 	fi
 }
 
